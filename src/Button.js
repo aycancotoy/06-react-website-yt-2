f0d67c08502cfb6d6ccb0edef552bdeba9c8181b
aycan <aycancotoy@gmail.com>
2021-11-05 15:28:14 +0300

    Navbar ve butonlar tamamen bitti.

--- src/Button.js
+++ src/Button.js
@@ -18,10 +18,10 @@ export const Button = ({
     const checkButtonStyle = STYLES.includes(buttonStyle) ? 
     buttonStyle : STYLES[0]
     
-    const checkButtonSize = STYLES.includes(buttonSize) ? 
+    const checkButtonSize = SIZES.includes(buttonSize) ? 
     buttonSize : SIZES[0]
 
-    const checkButtonColor = STYLES.includes(buttonColor) ? 
+    const checkButtonColor = COLOR.includes(buttonColor) ? 
     buttonColor : COLOR[4];
 
     return(
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { MdFingerprint } from "react-icons/md";
 import { FaTimes, FaWindows } from "react-icons/fa";
 import { GoThreeBars } from "react-icons/go";
 import { Button } from "../Button";
-import './Navbar.css';
+import "./Navbar.css";
+import { IconContext } from "react-icons/lib";
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -16,58 +17,64 @@ function Navbar() {
   const closeMobileMenu = () => setClick(false);
 
   const showButton = () => {
-      if(FaWindows.innerWidth <= 960) {
-          setButton(false)
-      } else {
-          setButton(true)
-      }
-  }
+    if (FaWindows.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
 
-  window.addEventListener('resize',showButton);
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
 
   return (
     <>
-      <div className="navbar">
-        <div className="navbar-container container">
-          <Link to="/" className="navbar-logo">
-            <MdFingerprint className="navbar-icon" />
-            LAVISH
-          </Link>
-          <div className="menu-icon" onClick={handleClick}>
-            {click ? <FaTimes /> : <GoThreeBars />}
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <div className="navbar">
+          <div className="navbar-container container">
+            <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+              <MdFingerprint className="navbar-icon" />
+              LAVISH
+            </Link>
+            <div className="menu-icon" onClick={handleClick}>
+              {click ? <FaTimes /> : <GoThreeBars />}
+            </div>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item">
+                <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/services" className="nav-links" onClick={closeMobileMenu}>
+                  Services
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/products" className="nav-links" onClick={closeMobileMenu}>
+                  Products
+                </Link>
+              </li>
+              <li className="nav-btn">
+                {button ? (
+                  <Link to="/sign-up" className="btn-link" onClick={closeMobileMenu}>
+                    <Button buttonStyle="btn--outline">SIGN UP</Button>
+                  </Link>
+                ) : (
+                  <Link to="/sign-up" className="btn-link" onClick={closeMobileMenu}>
+                    <Button buttonStyle="btn--outline" buttonSize="btn--mobile">
+                      SIGN UP
+                    </Button>
+                  </Link>
+                )}
+              </li>
+            </ul>
           </div>
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <Link to="/" className="nav-links">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/services" className="nav-links">
-                Services
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/products" className="nav-links">
-                Products
-              </Link>
-            </li>
-            <li className="nav-btn">
-              {button ? (
-                <Link to="/sign-up" className="btn-link">
-                  <Button buttonStyle="btn--outline">SIGN UP</Button>
-                </Link>
-              ) : (
-                <Link to="/sign-up" className="btn-link">
-                  <Button buttonStyle="btn--outline" buttonSize="btn--mobile">
-                    SIGN UP
-                  </Button>
-                </Link>
-              )}
-            </li>
-          </ul>
         </div>
-      </div>
+      </IconContext.Provider>
     </>
   );
 }
