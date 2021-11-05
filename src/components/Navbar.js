61c300e750d85b414324c192f3e0d10088d9fc7f
aycan <aycancotoy@gmail.com>
2021-11-05 14:17:41 +0300

    Navbar düzenlemeleri devam ediyor, içerikler girildi.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { MdFingerprint } from "react-icons/md";
-import { FaTimes } from 'react-icons/fa';
-import { GoThreeBars } from 'react-icons/go';
+import { FaTimes } from "react-icons/fa";
+import { GoThreeBars } from "react-icons/go";
 
 function Navbar() {
-    const [click, setClick] = useState(false);
+  const [click, setClick] = useState(false);
 
-    const handleClick = () => setClick(!click);
+  const handleClick = () => setClick(!click);
 
   return (
     <>
@@ -18,8 +18,38 @@ function Navbar() {
             LAVISH
           </Link>
           <div className="menu-icon" onClick={handleClick}>
-              {click ? <FaTimes /> : <GoThreeBars />}
+            {click ? <FaTimes /> : <GoThreeBars />}
           </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/services" className="nav-links">
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/products" className="nav-links">
+                Products
+              </Link>
+            </li>
+            <li className="nav-btn">
+              {button ? (
+                <Link to="/sign-up" className="btn-link">
+                  <Button buttonStyle="btn--outline">SIGN UP</Button>
+                </Link>
+              ) : (
+                <Link to="/sign-up" className="btn-link">
+                  <Button buttonStyle="btn--outline" buttonSize="btn--mobile">
+                    SIGN UP
+                  </Button>
+                </Link>
+              )}
+            </li>
+          </ul>
         </div>
       </div>
     </>
